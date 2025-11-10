f8de99e3520d8991ed71680e0f97b197f6f442ce
kjh5848 <kjh5848@gmail.com>
2025-11-11 00:02:01 +0900

    레디스설정변경

--- src/main/java/com/metacoding/spring_session_share_app2/HomeController.java
+++ src/main/java/com/metacoding/spring_session_share_app2/HomeController.java
@@ -4,20 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-public class SessionController {
+public class HomeController {
 
-    @GetMapping("/app2/")
-    public String home(HttpSession session, Model model) {
+    @GetMapping("/")
+    public String index(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession();
+
         Integer count = (Integer) session.getAttribute("count");
-        if (count == null)
-            count = 0;
-        count++;
+        count = (count == null) ? 1 : count + 1;
         session.setAttribute("count", count);
 
-        model.addAttribute("server", "App2 Server");
+        model.addAttribute("server", "app2"); // app2는 "app2"
+        model.addAttribute("sessionId", session.getId());
         model.addAttribute("count", count);
 
         return "index";
